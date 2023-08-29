ff07248013ecb2f4a41e38b39515189688afb8e1
Batuhan Yaşar Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-08-29 09:52:45 +0300

    modified:   libs/getPostMetadata.js

---
+++ libs/getPostMetadata.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import matter from "gray-matter";
+
+export const getPostMetadata = () => {
+    const folder = "posts/";
+    const files = fs.readdirSync(folder);
+    const markdownPosts = files.filter((file) => file.endsWith(".md"));
+    const posts = markdownPosts.map((filename) => {
+        const fileContents = fs.readFileSync(`posts/${filename}`, "utf8");
+        const matterResult = matter(fileContents);
+        return {
+            title: matterResult.data.title,
+            author: matterResult.data.author,
+            date: matterResult.data.date,
+            slug: filename.replace(".md", ""),
+            content: matterResult.content,
+        };
+    });
+    return posts;
+}
