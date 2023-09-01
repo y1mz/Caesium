b1d37104707bf673ad100e30f7acd0e80dd7df2c
Batuhan Y. Yılmaz <70820809+B4tuhanY1lmaz@users.noreply.github.com>
2023-09-01 21:44:21 +0300

    Removing unecessary stuff

---
+++ app/feed.xml/route.js
@@ -0,0 +1,55 @@
+import { Feed } from "feed";
+import Showdown from "showdown";
+import { getPostMetadata } from "@/libs/getPostMetadata";
+
+export async function GET() {
+
+    var converter = new Showdown.Converter();
+
+    const mtoh = ({ content }) => {
+        const text = content;
+        const html = converter.makeHtml(text);
+
+        return html;
+    };
+
+    const metadata = {
+        title: "TBNMC",
+        author: "TBNMC",
+        description: "TBNMC",
+        url: "www.tbnmc.xyz",
+    };
+
+    const postMetadataReversed = getPostMetadata();
+    const postMetadata = postMetadataReversed.slice(-10).reverse();
+    const posts = postMetadata;
+
+    const feed = new Feed({
+        title: metadata.title,
+        description: metadata.description,
+        link: metadata.url,
+        favicon: `${metadata.url}/logo.png`,
+        image: `${metadata.url}/logo.png`,
+        copyright: `GPLv3 ${new Date().getFullYear()}`,
+        updated: new Date(),
+        generator: `Manually typed by Batuhan Y. Yilmaz`,
+        feedLinks: {
+            rss2: `${metadata.url}/feed.xml`
+        },
+    });
+
+    posts.map((post) => {
+        const url = `${metadata.url}/blog/${post.slug}`;
+
+        feed.addItem({
+            title: post.title,
+            id: url,
+            link: url,
+            content: mtoh({ content: post.content }),
+            author: post.author,
+        });
+    });
+
+    return new Response(feed.rss2());
+
+}
